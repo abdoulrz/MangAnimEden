d8638fc291e4117890be71b2b76f96eef0f28904
AbdoulRZ <abdoulrazackzemane@gmail.com>
2026-02-10 17:39:54 +0100

    Final production settings and optimized build

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=yy7+4k^aw!u^b5v)!^b_*!sjx6*1wrocj^h=faz7n!rp=jnv'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l=yy7+4k^aw!u^b5v)!^b_*!sjx6*1wrocj^h=faz7n!rp=jnv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
     'manganimeden.web.app',
