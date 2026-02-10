315beae3e13a9c4b58477ffa95f57fea66eb4ee8
AbdoulRZ <abdoulrazackzemane@gmail.com>
2026-02-10 23:06:40 +0100

    Fix: Explicitly load .env variables using python-decouple for PythonAnywhere

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l=yy7+4k^aw!u^b5v)!^b_*!sjx6*1wrocj^h=faz7n!rp=jnv')
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='django-insecure-l=yy7+4k^aw!u^b5v)!^b_*!sjx6*1wrocj^h=faz7n!rp=jnv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '.web.app',
