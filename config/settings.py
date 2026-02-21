38e0af202a1c9450e7b80face2a02e6be656af93
AbdoulRZ <abdoulrazackzemane@gmail.com>
2026-02-21 16:30:46 +0100

    c

--- config/settings.py
+++ config/settings.py
@@ -242,7 +242,7 @@ SESSION_COOKIE_SECURE = not DEBUG  # True in prod (HTTPS), False in dev (HTTP)
 CSRF_COOKIE_SECURE = not DEBUG
 
 # Versioning for cache busting
-STATIC_VERSION = '2.7.18'
+STATIC_VERSION = '2.7.19'
 
 # Admin bootstrap via secret passphrase on registration
 ADMIN_BOOTSTRAP_PASSPHRASE = config('ADMIN_BOOTSTRAP_PASSPHRASE', default='Nefe')
