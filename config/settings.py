4be4800a88054cbc17de77d0be9b46bd17da9966
AbdoulRZ <abdoulrazackzemane@gmail.com>
2026-04-26 16:52:15 +0200

    Integration de la pub

--- config/settings.py
+++ config/settings.py
@@ -389,7 +389,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
 EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@manganimeden.net')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='MangAnimEden <noreply@manganimeden.net>')
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 
